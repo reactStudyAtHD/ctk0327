a0950934c69a6829eb51427199a51b13bc769981
ctk0327 <ctk0327@naver.com>
2020-03-22 16:11:31 +0900

    before eject

---
+++ styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent/>
+    </div>
+  );
+}
+
+export default App;
