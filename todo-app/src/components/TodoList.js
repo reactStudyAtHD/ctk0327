3a2ee7f34e5cbd7569f52583423188fde1ba6a59
ChunTaeKyung <ctk0327@naver.com>
2020-03-23 17:19:36 +0900

    End of Chapter10

---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import './TodoList.scss';
+
+const TodoList =({todos,onRemove,onToggle})=>{
+  return (
+      <div className="TodoList">
+          {todos.map(todo=>(
+              <TodoListItem onRemove={onRemove} onToggle={onToggle} todo={todo} key={todo.id} />
+          ))}
+          {/*<TodoListItem/>
+          <TodoListItem/>
+          <TodoListItem/>*/}
+      </div>
+  );
+};
+
+export default TodoList;
