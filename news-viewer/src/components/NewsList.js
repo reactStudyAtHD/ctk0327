22bd780c2d176ae558f08f7e065f3fb24432d7ec
ChunTaeKyung <ctk0327@naver.com>
2020-03-28 12:46:28 +0900

    End 14.5

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import NewsItem from './NewsItem';
+import axios from "axios";
 
 // const NewsListBlock
 
@@ -23,7 +24,8 @@ const sampleArticle = {
     urlToImage: 'https://via.placeholder.com/160',
 };
 
-const NewsList = () => {
+// END OF CHATPER 14.5
+/*const NewsList = () => {
     return (
         <NewsListBlock>
             <NewsItem article={sampleArticle}/>
@@ -34,6 +36,44 @@ const NewsList = () => {
             <NewsItem article={sampleArticle}/>
         </NewsListBlock>)
         ;
+};*/
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apikey=c59c6db75bd0445fa9e37c984fa42902'
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <NewsList>대기 중...</NewsList>;
+    }
+
+    if (!articles) {
+        return <div>머야</div>;
+    }
+
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article}/>
+                )
+            )}
+        </NewsListBlock>
+    );
 };
 
 export default NewsList;
