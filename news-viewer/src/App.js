316992c77b2e69ef5c319bce33c9ebef70aa309c
ChunTaeKyung <ctk0327@naver.com>
2020-03-30 02:37:43 +0900

    End of 14.7

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,7 +1,9 @@
 import React, {useCallback, useState} from 'react';
+import {Route} from 'react-router-dom';
 import axios from 'axios';
 import NewsList from "./components/NewsList";
 import Categories from "./components/Categories";
+import NewsPage from "./pages/NewsPage";
 /*
 const App = () => {
     const [data, setData] = useState(null);
@@ -30,10 +32,7 @@ const App =()=>{
     const [category,setCategory]=useState('all');
     const onSelect=useCallback(category=>setCategory(category),[]);
     return (
-        <>
-            <Categories category={category} onSelect={onSelect}/>
-            <NewsList category={category}/>
-        </>
+        <Route path={"/:category?"} component={NewsPage} />
     );
 };
 
