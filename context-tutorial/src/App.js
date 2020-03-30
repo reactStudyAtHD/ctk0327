0e1fb0aff706a2d226093a333f9fdb783cfd8574
ChunTaeKyung <ctk0327@naver.com>
2020-03-30 16:29:51 +0900

    End of Chapter 15.2

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+/*    <div className="App">
+      <ColorBox/>
+    </div>*/
+    <ColorContext.Provider value={{color: 'red'}}>
+        <div>
+            <ColorBox/>
+        </div>
+    </ColorContext.Provider>
   );
 }
 
